db9df813d5da3542df805d6e8b073c84b101d102
Yanuar <45526227+Yanuarramadhan@users.noreply.github.com>
2018-12-02 13:09:30 +0700

    codingannya
    include memakai iostream dengan int variabelnya yaitu(p,l,hasil)

---
+++ soal 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int p,l,Luas;
+
+    cout<<"masukan panjang persegi panjang:";
+    cin>>p;
+    cout<<"masukan lebar panjang:";
+    cin>>l;
+
+    Luas=p*l;
+    cout<<"luas persegi panjangnya yaitu"<<Luas;
+    return 0;
+}
